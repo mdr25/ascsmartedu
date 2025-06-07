dfa78f1c340ef35c2a4f7ef721c1cb73bd3546fd
ilsa margiana <ilsamargiana@gmail.com>
2025-06-07 23:42:54 +0700

    Update pages/admin/users

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/public";
 import PublicLayout from "./layouts/public";
+import AdminUsers from "./pages/admin/users";
+import AdminLayout from "./layouts/admin";
+import UserCreate from "./pages/admin/users/create";
 
 
 function App() {
@@ -12,7 +15,17 @@ function App() {
           <Route element={<PublicLayout />}>
             <Route index element={<Home />} />
           </Route>
-        
+
+          {/* admin */}
+          <Route path="admin" element={<AdminLayout />}>
+            {/* <Route index element={<Dashboard />} /> */}
+
+            <Route path="users">
+              <Route index element={<AdminUsers />} />
+              <Route path="create" element={<UserCreate />} />
+            </Route>
+          </Route>
+
         </Routes>
       </BrowserRouter>
     </>
