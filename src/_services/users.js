f3baa711327947c00dc6f7ed7c4ad33a4ecaf071
iamnadiaim <137363282+iamnadiaim@users.noreply.github.com>
2025-06-12 21:12:56 +0700

    Update users.js

--- src/_services/users.js
+++ src/_services/users.js
@@ -1,11 +1,1 @@
-import api from "../api"; 
 
-export const createUser = async (data) => {
-  try {
-    const response = await api.post("/users", data);
-    return response.data;
-  } catch (error) {
-    // Optional: log atau kembalikan error detail
-    throw error.response?.data || error;
-  }
-};
